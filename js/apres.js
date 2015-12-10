36d6dbee8899c074e6a432a038716db0baf8d100
Clay McLeod <clay.l.mcleod@gmail.com>
2015-12-09 23:03:15 -0600

    some styling updates

--- js/apres.js
+++ js/apres.js
@@ -1,86 +1,84 @@
 /** Variables **/
 
-progress_start_slide = 0;
+progressStartSlide = 0;
 
 /** Callbacks **/
 
-hash_changed = function () {
-    render_current_slide()
-}
+hashChanged = function() {
+  renderCurrentSlide();
+};
 
-key_down = function (evt) {
-    current_slide = parseInt(window.location.hash.replace('#',''));
-    console.log(evt.which)
-    if (evt.which === 37 || evt.which === 38) {
-        if (current_slide > 0) {
-            window.location.hash = current_slide - 1;
-        }
-    } else if (evt.which === 39 || evt.which === 40) {
-        if (current_slide < ($('.slide').length - 1)) {
-            window.location.hash = current_slide + 1;
-        }
+keyDown = function(evt) {
+  currentSlide = parseInt(window.location.hash.replace('#',''));
+  console.log(evt.which);
+  if (evt.which === 37 || evt.which === 38) {
+    if (currentSlide > 0) {
+      window.location.hash = currentSlide - 1;
     }
-}
+  } else if (evt.which === 39 || evt.which === 40) {
+    if (currentSlide < ($('.slide').length - 1)) {
+      window.location.hash = currentSlide + 1;
+    }
+  }
+};
 
 /** Slides functions **/
 
-display_slides = function () {
-    $('.slides').show();
-}
+displaySlides = function() {
+  $('.slides').show();
+};
 
-render_current_slide = function() {
-    var current_slide = parseInt(window.location.hash.replace('#',''));
-    
-    $('.slide').each(function (index, val) {
-        if (index === current_slide) {
-            $(this).show()
+renderCurrentSlide = function() {
+  var currentSlide = parseInt(window.location.hash.replace('#',''));
 
-            if ($(this).hasClass('title-slide')) {
-                $('.slides-progress').width('100%');
-            } else if ($(this).hasClass('resting-slide')) {
-                $('.slides-progress').width('0%');
-            } else if (index >= progress_start_slide) {
-                current = (index - progress_start_slide + 1) / ($('.slide').length - progress_start_slide) * 100;
-                console.log(current+'%');
-                $('.slides-progress').animate({width: current+'%'}, 500)
-            }
-        } else {
-            $(this).hide();
-        }
-    }); 
-}
+  $('.slide').each(function(index, val) {
+    if (index === currentSlide) {
+      $(this).show();
+
+      if ($(this).hasClass('title-slide')) {
+        $('.slides-progress').width('100%');
+      } else if ($(this).hasClass('resting-slide') && !$(this).hasClass('final-slide')) {
+        $('.slides-progress').width('0%');
+      } else if (index >= progressStartSlide) {
+        current = (index - progressStartSlide + 1) / ($('.slide').length - progressStartSlide) * 100;
+        console.log(current + '%');
+        $('.slides-progress').animate({width: current + '%'}, 500);
+      }
+    } else {
+      $(this).hide();
+    }
+  });
+};
 
 onload = function() {
-    
-    /** Callbacks **/
 
-    $(window).on('hashchange', hash_changed)
-    $(window).on('keydown', key_down)
+  /** Callbacks **/
 
-    /** Default settings **/
+  $(window).on('hashchange', hashChanged);
+  $(window).on('keydown', keyDown);
 
-    if (window.location.hash == "") {
-        window.location.hash = '0'
+  /** Default settings **/
+
+  if (window.location.hash == '') {
+    window.location.hash = '0';
+  }
+
+  /** Index slides **/
+
+  foundStartSlide = false;
+
+  $('.slide').each(function(index, val) {
+    $(this).data('slide-index', index);
+
+    if (!foundStartSlide) {
+      if ($(this).hasClass('resting-slide') || $(this).hasClass('title-slide')) {
+        progressStartSlide += 1;
+      } else {
+        foundStartSlide = true;
+      }
     }
-    
-    /** Index slides **/
+  });
 
-    foundStartSlide = false;
-
-    $('.slide').each(function (index, val) {
-        $(this).data('slide-index', index)    
-        
-        if (!foundStartSlide) {
-            if ($(this).hasClass('resting-slide') || $(this).hasClass('title-slide')) {
-                progress_start_slide += 1;
-            } else {
-                foundStartSlide = true;
-            }
-        }
-    });
-
-    render_current_slide();
-    display_slides();
-}
-
-
+  renderCurrentSlide();
+  displaySlides();
+};
